01a3d7a562ea91bf0d119e2b2558d5f5053540ed
Loki99911 <nikita_kresik@i.ua>
2023-12-27 21:11:29 +0200

    fix add

--- src/components/BGComp/BGComp.tsx
+++ src/components/BGComp/BGComp.tsx
@@ -1,21 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "./index.css";
 
 export default function BGComp() {
-    
-    useEffect(() => {
-    const flashlight = document.querySelector(
-      ".flashlight"
-    ) as HTMLElement | null;
+  const refContainer = useRef<HTMLDivElement>(null);
+  //   const flashlight = document.querySelector(".flashlight") as HTMLElement;
 
-    if (flashlight) {
-      flashlight.style.background =
-        "radial-gradient(circle, transparent 40px, rgba(0, 0, 0, 0.98) 80px)";
+  useEffect(() => {
+    function changeBg(e: MouseEvent) {
+      refContainer.current.style.background = `radial-gradient(circle at ${
+        (e.pageX / refContainer.current.offsetWidth) * 100
+      }% ${
+        (e.pageY / refContainer.current.offsetHeight) * 100
+      }%, transparent 40px, rgba(0, 0, 0, 0.98) 80px)`;
     }
-    },[])
+    refContainer.current.addEventListener("mousemove", changeBg);
+    return () => {
+      refContainer.current.removeEventListener("mousemove", changeBg);
+    };
+  }, [refContainer.current]);
+
   return (
     <div className="back-ground-img">
-      <div className="flashlight"></div>
+      <div ref={refContainer} className="flashlight"></div>
     </div>
   );
 }
