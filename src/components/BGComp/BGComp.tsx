e5d08d83f79f59cc607ce6f22a6f891f8910cb8f
Loki99911 <nikita_kresik@i.ua>
2023-12-28 14:55:57 +0200

    error wrapper

--- src/components/BGComp/BGComp.tsx
+++ src/components/BGComp/BGComp.tsx
@@ -3,14 +3,16 @@ import "./index.css";
 import { useMediaRules } from "../../hooks/useMediaRules";
 
 export default function BGComp() {
-  const { isMobile, isDesktop } = useMediaRules();
+  const { isMobile, isTablet, isDesktop } = useMediaRules();
   const refContainer = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const container = refContainer.current!;
     function changeBg(e: MouseEvent) {
-      container.style.background = `radial-gradient(circle at ${(e.pageX / container.offsetWidth) * 100
-        }% ${(e.pageY / container.offsetHeight) * 100
-        }%, transparent 40px, rgba(0, 0, 0, 0.98) 80px)`;
+      container.style.background = `radial-gradient(circle at ${
+        (e.pageX / container.offsetWidth) * 100
+      }% ${
+        (e.pageY / container.offsetHeight) * 100
+      }%, transparent 40px, rgba(0, 0, 0, 0.98) 80px)`;
     }
 
     container.addEventListener("mousemove", changeBg);
@@ -31,7 +33,11 @@ export default function BGComp() {
           ></a>
         </div>
       )}
-      {isMobile && <p>I'm sorry, it's only available on PC for now. Comig soon!</p>}
+      {(isTablet || isMobile) && (
+        <div className="ErrorWrapper">
+          <p>I'm sorry, it's only available on PC for now. Comig soon!</p>
+        </div>
+      )}
     </>
   );
 }
