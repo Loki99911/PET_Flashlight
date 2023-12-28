6258cd1c511ed1723ca5dac2baa6658d2912f696
Loki99911 <nikita_kresik@i.ua>
2023-12-28 14:48:10 +0200

    add sorry

--- src/components/BGComp/BGComp.tsx
+++ src/components/BGComp/BGComp.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useRef } from "react";
 import "./index.css";
+import { useMediaRules } from "../../hooks/useMediaRules";
 
 export default function BGComp() {
+  const { isMobile, isDesktop } = useMediaRules();
   const refContainer = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const container = refContainer.current!;
     function changeBg(e: MouseEvent) {
-      container.style.background = `radial-gradient(circle at ${
-        (e.pageX / container.offsetWidth) * 100
-      }% ${
-        (e.pageY / container.offsetHeight) * 100
-      }%, transparent 40px, rgba(0, 0, 0, 0.98) 80px)`;
+      container.style.background = `radial-gradient(circle at ${(e.pageX / container.offsetWidth) * 100
+        }% ${(e.pageY / container.offsetHeight) * 100
+        }%, transparent 40px, rgba(0, 0, 0, 0.98) 80px)`;
     }
 
     container.addEventListener("mousemove", changeBg);
@@ -20,13 +20,18 @@ export default function BGComp() {
   }, []);
 
   return (
-    <div className="back-ground-img">
-      <div ref={refContainer} className="flashlight"></div>
-      <a
-        href="https://www.linkedin.com/in/mykyta-kresik/"
-        className="link"
-        title="Click to visit my Linkedin!"
-      ></a>
-    </div>
+    <>
+      {isDesktop && (
+        <div className="back-ground-img">
+          <div ref={refContainer} className="flashlight"></div>
+          <a
+            href="https://www.linkedin.com/in/mykyta-kresik/"
+            className="link"
+            title="Click to visit my Linkedin!"
+          ></a>
+        </div>
+      )}
+      {isMobile && <p>I'm sorry, it's only available on PC for now. Comig soon!</p>}
+    </>
   );
 }
